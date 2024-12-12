a118c98d4264c8d6e9621b9aa89ae04e847ce225
aaEropro <upto.nogud@proton.me>
2024-12-12 18:54:58 +0200

    QOL update to flashcards

--- pages/flashcard/flashcard.js
+++ pages/flashcard/flashcard.js
@@ -1,38 +1,83 @@
 let questions = JSON.parse(localStorage.getItem("flashcards_data"));
 let flashcardQuestionIndex = parseInt(localStorage.getItem("flashcards_question_index") || "0")
 let loaded = 0;
+let question = false;
 
 
 $(document).ready(function () {
-    react2Event();
+    loadQuestion()
+
+    $("#prev_question").off("click").on("click", function () {
+        flashcardQuestionIndex--;
+        loadQuestion();
+    });
+    $("#next_question").off("click").on("click", function () {
+        if ($("#next_question").text() == "NEXT") {
+            flashcardQuestionIndex++;
+            loadQuestion();
+        }
+        else if ($("#next_question").text() == "HOME") {
+            window.location.href = "../home/home.html";
+        }
+    });
 });
 
 
-function react2Event() {
+function toggleFullscreen() {
+    if (!document.fullscreenElement && document.documentElement.requestFullscreen)
+        document.documentElement.requestFullscreen();
+    else if (document.fullscreenElement) document.exitFullscreen();
+}
+
+
+function loadQuestion() {
     const card = document.getElementById("card");
 
     if (questions == null) {
         console.log("question null");
         return;
     }
-
-    if (loaded == 1 && card.classList == "question") {
-        card.innerText = questions[flashcardQuestionIndex].answer;
-        card.classList = "answer"
-        flashcardQuestionIndex++;
+    if (card == null) {
+        console.log("could not find card");
         return;
     }
 
-    if (flashcardQuestionIndex >= questions.length) {
-        console.log("index runaway");
-        window.location.href = "../home/home.html";
+    localStorage.setItem("flashcards_question_index", flashcardQuestionIndex)
+    card.innerHTML = questions[flashcardQuestionIndex].question;
+    card.classList = "question"
+    question = true;
+
+    if (flashcardQuestionIndex == 0) $("#prev_question").prop("disabled", true);
+    else $("#prev_question").prop("disabled", false);
+
+    if (flashcardQuestionIndex + 1 >= questions.length) $("#next_question").text("HOME");
+    else $("#next_question").text("NEXT");
+
+    return;
+}
+
+
+function loadAnswer() {
+    const card = document.getElementById("card");
+
+    if (questions == null) {
+        console.log("question null");
+        return;
+    }
+    if (card == null) {
+        console.log("could not find card");
         return;
     }
 
-    if (card.classList.length == 0 || card.classList == "answer") {
-        card.innerText = questions[flashcardQuestionIndex].question;
-        card.classList = "question"
-        loaded = 1;
-        return;
-    }
+    card.innerHTML = questions[flashcardQuestionIndex].answer;
+    card.classList = "answer"
+    question = false;
+    return;
+}
+
+
+function flipCard() {
+    if (!question) loadQuestion();
+    else loadAnswer();
+    return;
 }
---
+++ services/flashcard_randomizer_service.js
@@ -0,0 +1,34 @@
+function getQuestions(path, number_of_quizzes, callback) {
+    const converter = new showdown.Converter({ simpleLineBreaks: true });
+
+    fetch(path)
+        .then(response => response.json())
+        .then(data => {
+            data = data.map(elem => ({
+                ...elem,
+                question: converter.makeHtml(elem.question),
+                answer: converter.makeHtml(elem.answer)
+            }));
+
+            questionsOrder = shuffle(data);
+            questionsOrder = questionsOrder.slice(0, number_of_quizzes);
+
+            callback(questionsOrder); // sent the questions back
+        })
+        .catch(error => console.error('Error fetching questions:', error));
+}
+
+
+/**
+ * Fisher-Yates Shuffle function to shuffle the questions array
+ * @param {array} array the array that should be shuffled
+ * @returns a new, shuffled array
+ */
+function shuffle(array) {
+    let shuffledArray = array.slice();
+    for (let i = shuffledArray.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [shuffledArray[i], shuffledArray[j]] = [shuffledArray[j], shuffledArray[i]];
+    }
+    return shuffledArray;
+}
